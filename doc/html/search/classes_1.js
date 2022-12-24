d1e260c69d3efdf9c1b04cf285298ba3f30a6073
limuy <limuyang202011@163.com>
2022-12-24 08:37:57 +0800

    refactor(base):删除过时的cfg.hpp

--- doc/html/search/classes_1.js
+++ doc/html/search/classes_1.js
@@ -1,9 +1,8 @@
 var searchData=
 [
-  ['cfg_0',['cfg',['../classtrc_1_1utils_1_1cfg.html',1,'trc::utils']]],
-  ['class_5fheader_1',['class_header',['../classtrc_1_1_t_v_m__space_1_1class__header.html',1,'trc::TVM_space']]],
-  ['color_5fst_2',['color_st',['../classtrc_1_1color_1_1color__st.html',1,'trc::color']]],
-  ['compiler_5foption_3',['compiler_option',['../structtrc_1_1compiler_1_1compiler__option.html',1,'trc::compiler']]],
-  ['compiler_5fpublic_5fdata_4',['compiler_public_data',['../structtrc_1_1compiler_1_1compiler__public__data.html',1,'trc::compiler']]],
-  ['cpp_5flib_5',['cpp_lib',['../classtrc_1_1_t_v_m__space_1_1cpp__libs_1_1cpp__lib.html',1,'trc::TVM_space::cpp_libs']]]
+  ['class_5fheader_0',['class_header',['../classtrc_1_1_t_v_m__space_1_1class__header.html',1,'trc::TVM_space']]],
+  ['color_5fst_1',['color_st',['../classtrc_1_1color_1_1color__st.html',1,'trc::color']]],
+  ['compiler_5foption_2',['compiler_option',['../structtrc_1_1compiler_1_1compiler__option.html',1,'trc::compiler']]],
+  ['compiler_5fpublic_5fdata_3',['compiler_public_data',['../structtrc_1_1compiler_1_1compiler__public__data.html',1,'trc::compiler']]],
+  ['cpp_5flib_4',['cpp_lib',['../classtrc_1_1_t_v_m__space_1_1cpp__libs_1_1cpp__lib.html',1,'trc::TVM_space::cpp_libs']]]
 ];
--- doc/html/search/files_2.js
+++ doc/html/search/files_2.js
@@ -1,16 +1,15 @@
 var searchData=
 [
-  ['cfg_2ehpp_0',['cfg.hpp',['../cfg_8hpp.html',1,'']]],
-  ['code_5floader_2ecpp_1',['code_loader.cpp',['../code__loader_8cpp.html',1,'']]],
-  ['code_5floader_2eh_2',['code_loader.h',['../code__loader_8h.html',1,'']]],
-  ['color_2ecpp_3',['color.cpp',['../color_8cpp.html',1,'']]],
-  ['color_2eh_4',['color.h',['../color_8h.html',1,'']]],
-  ['compile_5fenv_2ecpp_5',['compile_env.cpp',['../compile__env_8cpp.html',1,'']]],
-  ['compile_5fenv_2eh_6',['compile_env.h',['../compile__env_8h.html',1,'']]],
-  ['compiler_2ecpp_7',['Compiler.cpp',['../_compiler_8cpp.html',1,'']]],
-  ['compiler_2eh_8',['Compiler.h',['../_compiler_8h.html',1,'']]],
-  ['compiler_5fdef_2ecpp_9',['compiler_def.cpp',['../compiler__def_8cpp.html',1,'']]],
-  ['compiler_5fdef_2eh_10',['compiler_def.h',['../compiler__def_8h.html',1,'']]],
-  ['ctree_5floader_2ecpp_11',['ctree_loader.cpp',['../ctree__loader_8cpp.html',1,'']]],
-  ['ctree_5floader_2eh_12',['ctree_loader.h',['../ctree__loader_8h.html',1,'']]]
+  ['code_5floader_2ecpp_0',['code_loader.cpp',['../code__loader_8cpp.html',1,'']]],
+  ['code_5floader_2eh_1',['code_loader.h',['../code__loader_8h.html',1,'']]],
+  ['color_2ecpp_2',['color.cpp',['../color_8cpp.html',1,'']]],
+  ['color_2eh_3',['color.h',['../color_8h.html',1,'']]],
+  ['compile_5fenv_2ecpp_4',['compile_env.cpp',['../compile__env_8cpp.html',1,'']]],
+  ['compile_5fenv_2eh_5',['compile_env.h',['../compile__env_8h.html',1,'']]],
+  ['compiler_2ecpp_6',['Compiler.cpp',['../_compiler_8cpp.html',1,'']]],
+  ['compiler_2eh_7',['Compiler.h',['../_compiler_8h.html',1,'']]],
+  ['compiler_5fdef_2ecpp_8',['compiler_def.cpp',['../compiler__def_8cpp.html',1,'']]],
+  ['compiler_5fdef_2eh_9',['compiler_def.h',['../compiler__def_8h.html',1,'']]],
+  ['ctree_5floader_2ecpp_10',['ctree_loader.cpp',['../ctree__loader_8cpp.html',1,'']]],
+  ['ctree_5floader_2eh_11',['ctree_loader.h',['../ctree__loader_8h.html',1,'']]]
 ];

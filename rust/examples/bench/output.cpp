5f42ddc9ac2ff4c6955d5e45f784bea365457c83
limuy <limuyang202011@163.com>
2024-03-10 15:23:48 +0800

    add bench

---
+++ rust/examples/bench/output.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+using namespace std;
+
+int main() {
+    for (int a = 0;a<=100000000;++a) {
+        printf("%d}", a);
+    }
+    return 0;
+}
---
+++ rust/examples/bench/output.py
@@ -0,0 +1,2 @@
+for i in range(100000000-1):
+    print(i, end="")

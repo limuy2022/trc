17342af839b6336a65c3e49df578aec681d7de8e
limuy <limuyang202011@163.com>
2024-04-13 15:44:03 +0800

    fix(cfg):use shadow instead magic value

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,11 +1,11 @@
 //! some constant values and configurations in trc
 
+use crate::build::{self};
 use std::{path::PathBuf, sync::OnceLock};
 
 pub const MAIN_MODULE_NAME: &str = "main";
 pub const FLOAT_OVER_FLOW_LIMIT: usize = 18;
-pub const VERSION: &str = "0.0.1";
-pub static VERSION_DESTRUCT: (u8, u8, u8) = (0, 0, 1);
+pub const VERSION: &str = build::PKG_VERSION;
 
 pub fn get_history_file() -> &'static Option<PathBuf> {
     static PATH: OnceLock<Option<PathBuf>> = OnceLock::new();
@@ -22,5 +22,19 @@ pub fn get_history_file() -> &'static Option<PathBuf> {
             .unwrap()
             .ends_with("/trc/.trc_history"));
         Some(config_file)
+    })
+}
+
+pub fn get_version_destruct() -> &'static (u8, u8, u8) {
+    static VERSION_SPLIT: OnceLock<(u8, u8, u8)> = OnceLock::new();
+    VERSION_SPLIT.get_or_init(|| {
+        let tmp: Vec<u8> = VERSION
+            .split('.')
+            .map(|s| s.parse::<u8>().unwrap())
+            .collect();
+        match tmp.as_slice() {
+            [first, second, third] => (*first, *second, *third),
+            _ => panic!("version is not right"),
+        }
     })
 }

8893d7f10997a9f67f63707df38218f80507af3d
limuy <limuyang202011@163.com>
2024-04-21 08:36:53 +0800

    tests(main):add black test

--- tests/test_all_examples.rs
+++ tests/test_all_examples.rs
@@ -1,4 +1,36 @@
 //! this test all examples files and check them output
 
+use std::fs::read_to_string;
+
+use assert_cmd::Command;
+
 #[test]
-pub fn test_examples() {}
+pub fn test_run_examples() {
+    // 遍历整个examples目录
+    let mut cmd = Command::cargo_bin("trc").unwrap();
+    for entry in std::fs::read_dir("examples").unwrap() {
+        let path = entry.unwrap().path();
+        if path.is_file() {
+            // 获取标准答案
+            let mut ans_path = path.clone();
+            ans_path.pop();
+            ans_path.push("expected_result");
+            ans_path.push(path.file_name().expect("not file name"));
+            ans_path.set_extension("txt");
+            println!("{}", ans_path.display());
+            let expected_res = read_to_string(ans_path).unwrap();
+            let assert = cmd.arg("run").arg(path);
+            assert.assert().success().stdout(expected_res);
+        }
+    }
+}
+
+#[test]
+pub fn test_compile_examples() {
+    let mut cmd = Command::cargo_bin("trc").unwrap();
+}
+
+#[test]
+pub fn test_wrong_code() {
+    let mut cmd = Command::cargo_bin("trc").unwrap();
+}
